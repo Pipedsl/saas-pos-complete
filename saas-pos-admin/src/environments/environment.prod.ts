ab71be7877f4e97635a015adfd0dfb44f0cc3e03
Pipedsl <h.navarrete@duocuc.cl>
2025-12-06 17:15:47 -0300

    Config: Actualizar URL de producción

--- saas-pos-admin/src/environments/environment.prod.ts
+++ saas-pos-admin/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
     production: true,
     // Esta URL la obtendremos de Railway en el siguiente paso.
     // Por ahora pon un placeholder.
-    apiUrl: 'https://tu-backend-en-railway.app'
+    apiUrl: 'https://saas-pos-api-production.up.railway.app'
 };
