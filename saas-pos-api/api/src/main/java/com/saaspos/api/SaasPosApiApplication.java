df78b54355e92ae6fc204d6df9aa9c07a7e5170b
Pipedsl <h.navarrete@duocuc.cl>
2026-01-06 22:38:40 -0300

    Fix localDate

--- saas-pos-api/api/src/main/java/com/saaspos/api/SaasPosApiApplication.java
+++ saas-pos-api/api/src/main/java/com/saaspos/api/SaasPosApiApplication.java
@@ -1,8 +1,11 @@
 package com.saaspos.api;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableScheduling
@@ -11,5 +14,11 @@ public class SaasPosApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SaasPosApiApplication.class, args);
 	}
+    @PostConstruct
+    public void init() {
+        // Forzar la zona horaria a Chile para toda la aplicación
+        TimeZone.setDefault(TimeZone.getTimeZone("America/Santiago"));
+        System.out.println("--- ZONA HORARIA CONFIGURADA: " + TimeZone.getDefault().getID() + " ---");
+    }
 
 }
